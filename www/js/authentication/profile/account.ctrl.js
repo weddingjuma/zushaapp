bf37dbd5b2d8f800ba910a47febea958c0d31317
Denzel Wamburuclear <denzjoseph@gmail.com>
2015-11-01 22:06:16 +0300

    added GET request to fetch user info

--- www/js/authentication/profile/account.ctrl.js
+++ www/js/authentication/profile/account.ctrl.js
@@ -1,19 +1,19 @@
 angular.module('app.account', ['lbServices', 'ionic'])
     .controller('AccountCtrl', function ($scope, $location, User, Tweet) {
         $scope.currentUser = User.getCurrent();
-        Tweet.count({ownerId: $scope.currentUser.id})
+    /**    Tweet.count({ownerId: $scope.currentUser.id})
             .$promise
             .then(function (res) {
                 $scope.currentUser.tweets = res.count;
             });
-
+**/
         /**
          * @name logout()
          * logout user and redirect to the login page
          */
         $scope.logout = function () {
             User.logout(function () {
-                $location.path('/login');
+                $location.path('/signin');
             });
         }
 
