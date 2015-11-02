0f399d3918c04715a3655c89488ec6953823aa21
Denzel Wamburu <denzjoseph@gmail.com>
2015-11-02 20:09:13 +0300

    Declared account module in main app

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  angular.module('app', ['ionic', 'ngCordova', 'LocalForageModule', 'lbServices', 'ngResource', 'app.login', 'app.register'])
+  angular.module('app', ['ionic', 'ngCordova', 'LocalForageModule', 'lbServices', 'ngResource', 'app.login', 'app.register', 'app.account'])
     .config(configure)
 //    .run(runBlock);
 .run(function(User, $ionicPlatform, $rootScope, $location, $ionicPopup) {
