f0b73833e97460d1e3e6acc00d8adf82a1dcb628
Denzel Wamburu <denzjoseph@gmail.com>
2015-11-02 21:24:04 +0300

    fixed baseurl for REST

--- www/js/pages/pages.ctrl.js
+++ www/js/pages/pages.ctrl.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  angular.module('app')
+  angular.module('app', ['PageSrv'])
     .controller('PagesCtrl', function($scope, Page){
       $scope.pages = Page.query();
     })
