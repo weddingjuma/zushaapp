091739cd895ae45d7c86d9cf9d48b3683a6a2170
Denzel Wamburu <denzjoseph@gmail.com>
2015-12-21 07:27:57 +0300

    updated platform libraries

--- platforms/android/assets/www/js/layout/menu.ctrl.js
+++ platforms/android/assets/www/js/layout/menu.ctrl.js
@@ -3,16 +3,12 @@
   angular.module('app')
     .controller('MenuCtrl', MenuCtrl);
 
-  function MenuCtrl($scope, $state, AuthSrv){
-    var vm = {};
-    $scope.vm = vm;
-
-    vm.logout = logout;
-
-    function logout(){
-      AuthSrv.logout().then(function(){
-        $state.go('login');
+  function MenuCtrl($scope, $state, $location, User){
+  // add a logout function for logout button in menu
+  $scope.logout = function () {
+      User.logout(function () {
+          $location.path('/signin');
       });
-    };
+  }
   }
 })();
