24646389548050c93bdf09857fc2e16703437876
Denzel Wamburu <denzjoseph@gmail.com>
2016-05-28 04:16:52 +0300

    Include report and reports function

---
+++ www/app/reports/report.ctrl.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  angular.module('app')
+    .controller('ReportCtrl', ReportCtrl);
+
+  function ReportCtrl($scope, Reports){
+  
+  }
+})();
---
+++ www/app/reports/reports.ctrl.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  angular.module('app')
+    .controller('ReportsCtrl', ReportsCtrl);
+
+  function ReportsCtrl($scope, Reports){
+
+  }
+})();
