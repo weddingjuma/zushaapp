a32d5e6dd9306710e2dde4264321a30b42417694
Denzel Wamburu <denzjoseph@gmail.com>
2016-05-28 07:23:22 +0300

    Update service for reporting form

--- www/app/reports/reports.ctrl.js
+++ www/app/reports/reports.ctrl.js
@@ -1,23 +1,33 @@
-(function(){
+(function() {
   'use strict';
   angular.module('app')
     .controller('ReportsCtrl', ReportsCtrl);
 
-  function ReportsCtrl($scope, Reports){
+  function ReportsCtrl($scope, Reports) {
+    $scope.reports = {};
 
     // do post
     $scope.PostReport = function() {
       // $scope.showToast();
-      $location.path('app.reports');
+      // $location.path('app.reports');
 
       Reports
         .create({
-          date : new Date().toJSON()
+          date: new Date().toJSON(),
+          numberplate: $scope.reports.numberplate,
+          sacco: $scope.reports.sacco,
+          from: $scope.reports.from,
+          to: $scope.reports.to,
+          driverstate: $scope.reports.driverstate,
+          carstate: $scope.reports.carstate,
+          problem: $scope.reports.problem
+
+
 
         })
         .$promise
         .then(function(err) {
-        console.log(err)
+          console.log(err)
         });
     };
   }
