c8beb0c2631da868144bee8fceeb83c21c8a1ba7
Denzel Wamburu <denzjoseph@gmail.com>
2016-05-29 14:39:55 +0300

    Fixed Report id page

--- www/app/reports/report.ctrl.js
+++ www/app/reports/report.ctrl.js
@@ -3,7 +3,18 @@
   angular.module('app')
     .controller('ReportCtrl', ReportCtrl);
 
-  function ReportCtrl($scope, Reports){
-  
+  function ReportCtrl($scope, Reports, $stateParams){
+
+    Reports
+        .find({filter: {where: {id: $stateParams.id}}})
+        .$promise
+        .then(
+        function (res) {
+            $scope.report = res[0];
+
+        },
+        function (err) {
+
+        });
   }
 })();
